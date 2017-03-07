8aca65672ccb9d2e995dd8a58a81b93ab160c030
Jonathan Sick <jsick@lsst.org>
2017-03-06 23:03:49 -0800

    Reduce Specification to bare functionality
    The functionality reduction is to fulfill the new Specification design
    from sqr-017.lsst.io.
    
    - remove dependencies (no longer used)
    - remove filters (will become a provenance query)
    - remove separate unit argument from __init__ (quantity *must* be an
      astropy Quantity now).
    - remove irrelevant tests from test_specification.py (NOTE: tests in
      other modules are likely to fail!).

--- python/lsst/validate/base/spec.py
+++ python/lsst/validate/base/spec.py
@@ -18,20 +18,8 @@ class Specification(QuantityAttributeMixin, JsonSerializationMixin):
     name : `str`
         Name of the specification level for a metric. LPM-17, for example,
         uses ``'design'``, ``'minimum'`` and ``'stretch'`` terminology.
-    quantity : `astropy.units.Quantity`, `float`, or `int`
+    quantity : `astropy.units.Quantity`
         The specification threshold level.
-    unit : `str`, optional
-        `astropy.units.Unit`-compatible `str` describing the units of
-        ``value`` (only necessary if `quantity` is a `float`).
-        An empty string (``''``) describes a unitless quantity.
-    filter_names : `list`, optional
-        A list of optical filter names that this specification applies to.
-        Set only if the specification level is dependent on the filter.
-    dependencies : `dict`, optional
-        A dictionary of named `Datum` values that must be known when making a
-        measurement against a specification level. Dependencies can be
-        accessed as attributes of the specification object. The names of
-        class attributes match keys in `dependencies`.
     """
 
     name = None
@@ -44,41 +32,9 @@ class Specification(QuantityAttributeMixin, JsonSerializationMixin):
     quantity = None
     """The specification threshold level (`astropy.units.Quantity`)."""
 
-    filter_names = None
-    """`list` of names of optical filters that this Specification level
-    applies to.
-
-    Default is `None` if the `Specification` is filter-independent.
-    """
-
-    dependencies = None
-    """`dict` of named `Datum` values that must be known when making a
-    measurement against a specification level.
-
-    Dependencies can also be accessed as attributes of the `Specification`
-    object. The names of class attributes match keys in `dependencies`.
-    """
-
-    def __init__(self, name, quantity, unit=None, filter_names=None,
-                 dependencies=None):
+    def __init__(self, name, quantity):
         self.name = name
-        if unit is not None:
-            self.quantity = quantity * u.Unit(unit)
-        else:
-            self.quantity = quantity
-        self.filter_names = filter_names
-        if dependencies:
-            self.dependencies = dependencies
-        else:
-            self.dependencies = {}
-
-    def __getattr__(self, key):
-        """Access dependencies with keys as attributes."""
-        if key in self.dependencies:
-            return self.dependencies[key]
-        else:
-            raise AttributeError("%r object has no attribute %r" %
-                                 (self.__class__, key))
+        self.quantity = quantity
 
     @property
     def datum(self):
@@ -100,12 +56,8 @@ class Specification(QuantityAttributeMixin, JsonSerializationMixin):
             Specification from JSON.
         """
         q = Datum._rebuild_quantity(json_data['value'], json_data['unit'])
-        deps = {k: Datum.from_json(v)
-                for k, v in json_data['dependencies'].items()}
         s = cls(name=json_data['name'],
-                quantity=q,
-                filter_names=json_data['filter_names'],
-                dependencies=deps)
+                quantity=q)
         return s
 
     @property
@@ -120,6 +72,4 @@ class Specification(QuantityAttributeMixin, JsonSerializationMixin):
         return JsonSerializationMixin.jsonify_dict({
             'name': self.name,
             'value': v,
-            'unit': self.unit_str,
-            'filter_names': self.filter_names,
-            'dependencies': self.dependencies})
+            'unit': self.unit_str})
--- tests/test_specification.py
+++ tests/test_specification.py
@@ -6,7 +6,7 @@ import unittest
 
 import astropy.units as u
 
-from lsst.validate.base import Specification, Datum
+from lsst.validate.base import Specification
 
 
 class SpecificationTestCase(unittest.TestCase):
@@ -35,32 +35,6 @@ class SpecificationTestCase(unittest.TestCase):
         s2 = Specification.from_json(json_data)
         self.assertEqual(s.name, s2.name)
         self.assertEqual(s.quantity, s2.quantity)
-        self.assertEqual(s.filter_names, s2.filter_names)
-
-        # test datum output
-        d = s.datum
-        self.assertEqual(d.quantity, 5. * u.mag)
-        self.assertEqual(d.label, 'design')
-
-    def test_from_value(self):
-        """Test creating a specification from a value."""
-        s = Specification('design', 5., unit='mag')
-        self.assertEqual(s.quantity.value, 5.)
-        self.assertEqual(s.unit, u.mag)
-        self.assertEqual(s.unit_str, 'mag')
-        self.assertEqual(s.name, 'design')
-
-        # test json output
-        json_data = s.json
-        self.assertEqual(json_data['name'], 'design')
-        self.assertEqual(json_data['value'], 5.)
-        self.assertEqual(json_data['unit'], 'mag')
-
-        # rebuild from json
-        s2 = Specification.from_json(json_data)
-        self.assertEqual(s.name, s2.name)
-        self.assertEqual(s.quantity, s2.quantity)
-        self.assertEqual(s.filter_names, s2.filter_names)
 
         # test datum output
         d = s.datum
@@ -69,9 +43,9 @@ class SpecificationTestCase(unittest.TestCase):
 
     def test_unitless(self):
         """Test unitless specifications."""
-        s = Specification('design', 100., unit='')
+        s = Specification('design', 100. * u.dimensionless_unscaled)
         self.assertEqual(s.quantity.value, 100.)
-        self.assertEqual(s.unit, u.Unit(''))
+        self.assertEqual(s.unit, u.dimensionless_unscaled)
         self.assertEqual(s.unit_str, '')
 
         # test json output
@@ -84,41 +58,11 @@ class SpecificationTestCase(unittest.TestCase):
         s2 = Specification.from_json(json_data)
         self.assertEqual(s.name, s2.name)
         self.assertEqual(s.quantity, s2.quantity)
-        self.assertEqual(s.filter_names, s2.filter_names)
 
         # test datum output
         d = s.datum
-        self.assertEqual(d.quantity, 100. * u.Unit(''))
+        self.assertEqual(d.quantity, 100. * u.dimensionless_unscaled)
         self.assertEqual(d.label, 'design')
-
-    def test_unitless_int(self):
-        """Test that a specification can be a unitless integer (count)."""
-        s = Specification('design', 10)
-        self.assertEqual(s.quantity, 10)
-
-        # test JSON output
-        json_data = s.json
-        self.assertEqual(json_data['value'], 10)
-
-        # rebuild from JSON
-        s2 = Specification.from_json(json_data)
-        self.assertEqual(s.quantity, s2.quantity)
-
-    def test_filters(self):
-        """Test setting filter dependencies."""
-        filter_names = ['u', 'g']
-        s = Specification('design', 5 * u.mag, filter_names=filter_names)
-        for filter_name in s.filter_names:
-            self.assertIn(filter_name, filter_names)
-        self.assertEqual(len(filter_names), len(s.filter_names))
-
-    def test_dependency_access(self):
-        deps = {'a': Datum(5., 'mag')}
-        s = Specification('design', 0., '', dependencies=deps)
-        self.assertEqual(s.a.quantity, 5. * u.mag)
-        json_data = s.json
-        self.assertEqual(json_data['dependencies']['a']['value'], 5.)
-        self.assertEqual(json_data['dependencies']['a']['unit'], 'mag')
 
 
 if __name__ == "__main__":
